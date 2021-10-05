d43da88f14ab851a119ad4217fffbb08bb4a1dfc
sanjayyadavgithub <48355724+sanjayyadavgithub@users.noreply.github.com>
2021-10-05 14:53:03 +0530

    commit

---
+++ build/precache-manifest.e8de0a9e53483d1bbe002d4485b70d91.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fe96991b1ac314dd0ed",
+    "url": "/static/css/main.306e3a90.chunk.css"
+  },
+  {
+    "revision": "9fe96991b1ac314dd0ed",
+    "url": "/static/js/main.67a5e265.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "623800016476bf10924e",
+    "url": "/static/js/2.8273c62d.chunk.js"
+  },
+  {
+    "revision": "d1369ee693274ebace8e79c13f8b8d38",
+    "url": "/static/media/backCardImg.d1369ee6.png"
+  },
+  {
+    "revision": "69b3940bd4ae7bfd8e8652dcd57e7ced",
+    "url": "/static/media/heart.69b3940b.png"
+  },
+  {
+    "revision": "a5654b42a5eea0b85c879693a815c6d2",
+    "url": "/static/media/diamond.a5654b42.png"
+  },
+  {
+    "revision": "2a6241117edd8c869a89d07c1f0232c7",
+    "url": "/static/media/club.2a624111.png"
+  },
+  {
+    "revision": "c9111145773fb0dd7bfc2d5d55bff327",
+    "url": "/static/media/spade.c9111145.png"
+  },
+  {
+    "revision": "58d2fbb37b35795d7102ed863eefae1e",
+    "url": "/index.html"
+  }
+];
